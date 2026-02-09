6b46132c8de98f5e33383123125815333593c632
john-dowds <jdowds@nightshift-interactive.com>
2026-02-09 09:16:06 -0600

    Update firebase-init.js

--- assets/js/firebase-init.js
+++ assets/js/firebase-init.js
@@ -5,21 +5,19 @@ import { initializeApp } from "https://www.gstatic.com/firebasejs/12.8.0/firebas
 import { getAuth } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-auth.js";
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-firestore.js";
 import { getFunctions } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-functions.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/12.8.0/firebase-database.js"; 
 
 const firebaseConfig = {
-    apiKey: "AIzaSyBuh70Dl0RiHtxIfAGI2yfbMIHD_di6LGA",
-    authDomain: "elrendar-fellowship.firebaseapp.com",
-    projectId: "elrendar-fellowship",
-    storageBucket: "elrendar-fellowship.firebasestorage.app",
-    messagingSenderId: "300802961252",
-    appId: "1:300802961252:web:c683ed87cc6b4ed096d471"
-  };
+  apiKey: "AIzaSyBuh70Dl0RiHtxIfAGI2yfbMIHD_di6LGA",
+  authDomain: "elrendar-fellowship.firebaseapp.com",
+  projectId: "elrendar-fellowship",
+  storageBucket: "elrendar-fellowship.firebasestorage.app",
+  messagingSenderId: "300802961252",
+  appId: "1:300802961252:web:c683ed87cc6b4ed096d471"
+};
 
 export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
 export const db = getFirestore(app);
 export const functions = getFunctions(app, "us-central1");
-
-
-
-
+export const rtdb = getDatabase(app);
